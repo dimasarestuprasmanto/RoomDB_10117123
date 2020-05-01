24a59fae1834e8c858a4731e553be772eae6d581
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2020-05-01 15:07:29 +0700

    Initial commit

---
+++ app/src/main/java/com/dimasarp/roomdb_10117123/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.dimasarp.roomdb_10117123;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract AktivisDao aktivisDao();
+}
